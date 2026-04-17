35262a6567c61dd260e60ec02c1c35908fd662ab
Sascha Janiszewski <sascha.janiszewski@googlemail.com>
2026-04-17 11:24:40 +0200

    changed the string of trip_duration_stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -84,7 +84,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays statistics on the total and average trip duration for you."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
