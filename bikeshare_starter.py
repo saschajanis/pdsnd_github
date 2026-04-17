808ef45920c232b957062facae7615fdc0cf7d88
Sascha Janiszewski <sascha.janiszewski@googlemail.com>
2026-04-17 11:26:42 +0200

    changed the print of station_stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -67,7 +67,7 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCalculating The Most Popular Stations and Trip are...\n')
     start_time = time.time()
 
     # display most commonly used start station
